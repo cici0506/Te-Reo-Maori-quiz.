a1dab1c5c063021f233974ff50efa34a029665db
Cindy Kuang <ac117108@avcol.school.nz>
2022-03-21 13:09:59 +1300

    Changed repetitive if statement to if, else if and else. Added more questions to level 1 and added some questions to level 3

--- Te Reo Maori quiz/Program.cs
+++ Te Reo Maori quiz/Program.cs
@@ -11,9 +11,19 @@ namespace MaoriQuiz
             string answer3 = "";
             string answer4 = "";
             string answer5 = "";
+            string answer6 = "";
+            string answer7 = "";
+            string answer8 = "";
+            string answer9 = "";
+            string answer10 = "";
+            string answer11 = "";
+            string answer12 = "";
+            string answer13 = "";
+            string answer14 = "";
+            string answer15 = "";
 
-            /*This is an array to repreent a fixed length ordered collection of values with the same type to indicate that this is a three leveled quiz.
-             * I used the array so it makes it easier to organize and operate on large amounts of data. So instead of creating mulitple variables, I can just create one array that stores it all. */  
+            /* This is an array to repreent a fixed length ordered collection of values with the same type to indicate that this is a three leveled quiz.
+             * I used the array so it makes it easier to organize and operate on large amounts of data. So instead of creating mulitple variables, I can just create one array that stores it all. */
             string[] test = { "1", "2", "3" };
 
             //introduction
@@ -32,7 +42,7 @@ namespace MaoriQuiz
 
                 //introduction
 
-                Console.WriteLine("Welcome " + name + " this program is a 15 question Teo Reo Maori quiz.\n\n");
+                Console.WriteLine("Welcome " + name + " this program is a 45 question Teo Reo Maori quiz.\n\n");
 
 
                 Console.WriteLine("There are three levels, please enter the number you wish to do\n1. Basic\n2. Intermediate\n3. Advanced\n"); 
@@ -115,11 +125,141 @@ namespace MaoriQuiz
                             Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
 
                         }
+                        Console.WriteLine("Question 6\nWhat does 'love' mean in Maori " + name + "?\na.te aroha\nb.pai\nc.aroha\n");
+                        answer6 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer6 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 7\nWhat does 'tribe' mean in Maori " + name + "?\na.ropu matawaka\nb.iwi\nc.whare rangatira\n");
+                        answer7 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer7 == "b")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 8\nWhat does 'see you later' mean in Maori " + name + "?\na.ka kite ano\nb.poroporoaki\nc.kia roa\n");
+                        answer8 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer8 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 9\nWhat does 'sit' mean in Maori " + name + "?\na.nohoanga\nb.okiokinga\nc.e noho\n");
+                        answer9 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer9 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 10\nWhat does 'welcome' mean in Maori " + name + "?\na.nau mai\nb.tena koutou\nc.whakaaetanga\n");
+                        answer10 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer10 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 11\nWhat does 'food' mean in Maori " + name + "?\na.kai\nb.whangai\nc.tunu kai\n");
+                        answer11 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer11 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 12\nWhat does 'canoe' mean in Maori " + name + "?\na.waka\nb.poti\nc.waka waka\n");
+                        answer12 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer12 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 13\nWhat does 'mountain' mean in Maori " + name + "?\na.teitei\nb.tihi\nc.maunga\n");
+                        answer13 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer13 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 14\nWhat does 'good morning' mean in Maori " + name + "?\na.ata\nb.morena\nc.awatea\n");
+                        answer14 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer14 == "b")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
+                        Console.WriteLine("Question 15\nWhat does 'ten' mean in Maori " + name + "?\na.tekau\nb.whakatekau\nc.hekaono\n");
+                        answer15 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer15 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+                        }
                         end();
                     }
 
 
-                    if (userLevel == "2")
+                    else if (userLevel == "2")
                     {
 
                         Console.WriteLine("Question 1\nWhat does 'chief' mean in Maori " + name + "?\na.tumuaki\nb.rangatira\nc.kapene\n");
@@ -137,7 +277,7 @@ namespace MaoriQuiz
                         Console.WriteLine("Question 2\nWhat does 'sacred' mean in Maori " + name + "?\na.manaakitia\nb.tapu\nc.arohaina\n");
                         answer2 = Console.ReadLine();
                         Console.Clear();
-                        if (answer2 == "a")
+                        if (answer2 == "b")
                         {
                             score = score + 1;
                             Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
@@ -185,12 +325,150 @@ namespace MaoriQuiz
                             Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
 
 
+                        }  Console.WriteLine("Question 6\nWhat does 'children' mean in Maori " + name + "?\na.uri\nb.tamariki\nc.iti\n");
+                        answer6 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer6 == "b")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 7\nWhat does 'elder' mean in Maori " + name + "?\na.kaumatua\nb.tangata pakeke\nc.tupuna\n");
+                        answer7 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer7 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 8\nWhat does 'prayer' mean in Maori " + name + "?\na.wawao\nb.inoi\nc.karakia\n");
+                        answer8 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer8 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 9\nWhat does 'work' mean in Maori " + name + "?\na.mahi\nb.ratonga\nc.taumahi\n");
+                        answer9 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer9 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 10\nWhat does 'stomach' mean in Maori " + name + "?\na.whekau\nb.puku\nc.puku puku\n");
+                        answer10 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer10 == "b")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 11\nWhat does 'water' mean in Maori " + name + "?\na.maku\nb.inu\nc.wai\n");
+                        answer11 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer11 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 12\nWhat does 'saturday' mean in Maori " + name + "?\na.okiokinga\nb.wiki\nc.rahoroi\n");
+                        answer12 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer12 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 13\nWhat does 'listen' mean in Maori " + name + "?\na.whakarongo\nb.rongo\nc.aro mai\n");
+                        answer14 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer15 == "a")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 14\nWhat does 'thank you' mean in Maori " + name + "?\na.whakaae\nb.mihi\nc.maioha\n");
+                        answer14 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer14 == "b")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
+                        }
+                        Console.WriteLine("Question 15\nWhat does 'talk' mean in Maori " + name + "?\na.korerorero\nb.kohukohu\nc.korero\n");
+                        answer15 = Console.ReadLine();
+                        Console.Clear();
+                        if (answer15 == "c")
+                        {
+                            score = score + 1;
+                            Console.WriteLine("Correct!\n" + name + "'s score:" + score + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
+
+
                         }
                         end();
                     }
-                    if (userLevel == "3")
-
-
+                   else
+                    
                         Console.WriteLine("Question 1\nWhat does 'genealogy' mean in Maori " + name + "?\na.takenga\nb.iranga\nc.whakapapa\n");
                     answer1 = Console.ReadLine();
                     Console.Clear();
@@ -216,7 +494,7 @@ namespace MaoriQuiz
                         Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
                     }
 
-                    Console.WriteLine("Question 3\nWhat does 'birthday' mean in Maori " + name + "?\na.whanautanga\nb.huritau\nc.\n");
+                    Console.WriteLine("Question 3\nWhat does 'birthday' mean in Maori " + name + "?\na.whanautanga\nb.huritau\nc.whakanui\n");
                     answer3 = Console.ReadLine();
                     Console.Clear();
                     if (answer3 == "b")
@@ -228,7 +506,7 @@ namespace MaoriQuiz
                     {
                         Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
                     }
-                    Console.WriteLine("Question 4\nWhat  " + name + "?\na.\nb.\nc.\n");
+                    Console.WriteLine("Question 4\nWhat does 'thunderstorm' mean in Maori " + name + "?\na.whatitiri\nb.awha\nc.ua\n");
                     answer4 = Console.ReadLine();
                     Console.Clear();
                     if (answer4 == "a")
@@ -241,7 +519,7 @@ namespace MaoriQuiz
                         Console.WriteLine("Wrong!\n" + name + "'s score:" + score + "\n");
 
                     }
-                    Console.WriteLine("Question 5\nWhat  " + name + "?\na.\nb.\nc.\n");
+                    Console.WriteLine("Question 5\nWhat does 'sincerity' mean in Maori " + name + "?\na.ngakau nui\nb.pono\nc.whakapono pai\n");
                     answer5 = Console.ReadLine();
                     Console.Clear();
                     if (answer5 == "b")
